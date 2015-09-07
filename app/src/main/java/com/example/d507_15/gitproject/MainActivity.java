305cd6936e246bb7653233332b5d829cc231efd1
TaeYoung-Ahn <tyoung.an@gmail.com>
2015-09-07 10:51:13 +0900

    Button

--- app/src/main/java/com/example/d507_15/gitproject/MainActivity.java
+++ app/src/main/java/com/example/d507_15/gitproject/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
